54871272b6c425a590515e06b3c39a1ee6e61c8c
Yuriy Gelvih <Gelvihurij@gmail.com>
2023-04-02 01:58:01 +0700

    Adding Program and gitignore

---
+++ Program/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] GenerateArray()
+{
+    System.Console.WriteLine("Пожалуйста заполните массив.");
+    string[] arr = new string[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine("Введите новый элемент массива и нажмите Enter.");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        System.Console.Write(arr[i] + ", ");
+    }
+    System.Console.WriteLine(arr[^1]);
+}
+
+string[] NewArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3) count++;
+    }
+    string[] newArr = new string[count];
+
+    count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3) 
+        {
+            newArr[count] = arr[i];
+            count++;
+        }
+    }
+    return newArr;
+}
+
+string[] myArray = GenerateArray();
+System.Console.WriteLine("Массив из введённых вами строк: ");
+PrintArray(myArray);
+string[] newArray = NewArray(myArray);
+System.Console.WriteLine("Массив из строк, длинна которых меньше либо равна трём: ");
+PrintArray(newArray);
